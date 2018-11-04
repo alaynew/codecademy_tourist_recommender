e40c88fa1a8fbc5cea637a165464336c8c9d67a4
Alayne W <awiinsky@gmail.com>
2018-11-04 15:56:27 -0700

    define first lists for test

--- tourist_recommender.py
+++ tourist_recommender.py
@@ -1,1 +1,5 @@
-#test
+
+#list of destinations
+destinations = ["Paris, France", "Shanghai,China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
+#testing with an initial traveler profile
+test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
