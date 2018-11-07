c3b4580044574628077f7bf9445fb9969649d234
Alayne W <awiinsky@gmail.com>
2018-11-06 23:42:54 -0700

    Add logic to find traveler destinations and convert to internal data

--- tourist_recommender.py
+++ tourist_recommender.py
@@ -3,3 +3,20 @@
 destinations = ["Paris, France", "Shanghai,China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 #testing with an initial traveler profile
 test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+
+#identify locations based on its index in destinations list
+def get_destination_index(destination):
+  destination_index = destinations.index(destination)
+  return destination_index
+#print(get_destination_index("Los Angeles, USA"))
+#print(get_destination_index("Paris, France"))
+#print(get_destination_index("Hyderabad,India"))
+
+#get traveler's location
+def get_traveler_location(traveler):
+    traveler_destination = traveler[1]
+    traveler_destination_index = get_destination_index(traveler_destination)
+    return traveler_destination_index
+
+test_destination_index = get_traveler_location(test_traveler)
+print(test_destination_index)
