5dec0823d8b4148879b4bf0a1bca115d84f7026d
Alayne W <awiinsky@gmail.com>
2018-11-06 23:56:07 -0700

    Fix space error in destinations list

--- tourist_recommender.py
+++ tourist_recommender.py
@@ -2,7 +2,7 @@
 #list of destinations
 destinations = ["Paris, France", "Shanghai,China", "Los Angeles, USA", "São Paulo, Brazil", "Cairo, Egypt"]
 #testing with an initial traveler profile
-test_traveler = ['Erin Wilkes', 'Shanghai, China', ['historical site', 'art']]
+test_traveler = ['Erin Wilkes', 'Shanghai,China', ['historical site', 'art']]
 
 #identify locations based on its index in destinations list
 def get_destination_index(destination):
